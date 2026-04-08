f8920b6d21566711c6ee15fc75ec14b56b83be25
Smita Bharat <40206905+smitabharat@users.noreply.github.com>
2026-04-08 22:21:08 +0530

    Create reverseWord.go
    function to reverse single word

---
+++ reverseWord.go
@@ -0,0 +1,8 @@
+// Function to reverse a single word
+func reverseWord(word string) string {
+	runes := []rune(word)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
